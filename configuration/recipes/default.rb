1da78d79d8ebd12a7e9d5e8b46203479964afa15
Robert <rquinones93@yahoo.com>
2019-04-01 02:48:10 -0700

    Finished OS portion of linux baseline as much as I could

--- configuration/recipes/default.rb
+++ configuration/recipes/default.rb
@@ -4,8 +4,5 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-package 'apache2'
-
-file '/home/cheftest/testfile.txt' do
-  content 'CHECKING TO SEE!'
-end
+# Run List - These recipes run in order
+include_recipe 'configuration::os_spec'
