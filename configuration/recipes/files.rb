c2d353fcb08b922984b24648bf9c4dc63819b14e
Robert <rquinones93@yahoo.com>
2019-05-07 17:26:17 -0700

    Did a bunch

--- configuration/recipes/files.rb
+++ configuration/recipes/files.rb
@@ -5,35 +5,29 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 # Deven
-file 'testfile.txt' do 
+file '/home/deven/testfile.txt' do 
 	content	'Test File Content'
-	path	'/home/deven/testfile.txt'
 end
 
-file 'index.html' do 
+file '/home/deven/Documents/index.html' do 
 	content	'<p>Hello World!</p>'
-	path	'/home/deven/Documents/index.html'
 end
 
-file 'todo_notes.txt' do 
+file '/home/deven/Desktop/todo_notes.txt' do 
 	content	`Don't forget to finish the project.`
-	path	'/home/deven/Desktop/todo_notes.txt'
 end
 
-file 'reading_list.txt' do 
+file '/home/deven/Downloads/reading_list.txt' do 
 	content	'What is the SysAdmin Textbook?'
-	path	'/home/deven/Downloads/reading_list.txt'
 end
 
 # Robert
-file 'testfile.txt' do 
+file '/home/robert/testfile.txt' do 
 	content	'Test File Content'
-	path	'/home/robert/testfile.txt'
 end
 
-file 'index.html' do 
+file '/home/robert/Documents/index.html' do 
 	content	'<p>Hello World!</p>'
-	path	'/home/robert/Documents/index.html'
 end
 
 file 'todo_notes.txt' do 
@@ -41,66 +35,57 @@ file 'todo_notes.txt' do
 	path	'/home/robert/Desktop/todo_notes.txt'
 end
 
-file 'reading_list.txt' do 
+file '/home/robert/Downloads/reading_list.txt' do 
 	content	'What is the SysAdmin Textbook?'
-	path	'/home/robert/Downloads/reading_list.txt'
 end
 
 # Dawit
-file 'testfile.txt' do 
+file '/home/dawit/testfile.txt' do 
 	content	'Test File Content'
-	path	'/home/dawit/testfile.txt'
 end
 
-file 'index.html' do 
+file '/home/dawit/Documents/index.html' do 
 	content	'<p>Hello World!</p>'
-	path	'/home/dawit/Documents/index.html'
 end
 
-file 'todo_notes.txt' do 
+file '/home/dawit/Desktop/todo_notes.txt' do 
 	content	`Don't forget to finish the project.`
-	path	'/home/dawit/Desktop/todo_notes.txt'
 end
 
-file 'reading_list.txt' do 
+file '/home/dawit/Downloads/reading_list.txt' do 
 	content	'What is the SysAdmin Textbook?'
-	path	'/home/dawit/Downloads/reading_list.txt'
 end
 
 # Andrew
-file 'testfile.txt' do 
+file '/home/andrew/testfile.txt' do 
 	content	'Test File Content'
-	path	'/home/andrew/testfile.txt'
 end
 
-file 'index.html' do 
+file '/home/andrew/Documents/index.html' do 
 	content	'<p>Hello World!</p>'
-	path	'/home/andrew/Documents/index.html'
 end
 
-file 'todo_notes.txt' do 
+file '/home/andrew/Desktop/todo_notes.txt' do 
 	content	`Don't forget to finish the project.`
-	path	'/home/andrew/Desktop/todo_notes.txt'
 end
 
-file 'reading_list.txt' do 
+file '/home/andrew/Downloads/reading_list.txt' do 
 	content	'What is the SysAdmin Textbook?'
-	path	'/home/andrew/Downloads/reading_list.txt'
 end
 
 # Templates
-template '/home/deven/testTemplate.txt' do
-	source	'test.erb'
+template '/home/deven/testtemplate.txt' do
+	source	'test.txt.erb'
 end
 
-template '/home/robert/testTemplate.txt' do
-	source	'test.erb'
+template '/home/robert/testtemplate.txt' do
+	source	'test.txt.erb'
 end
 
-template '/home/dawit/testTemplate.txt' do
-	source	'test.erb'
+template '/home/dawit/testtemplate.txt' do
+	source	'test.txt.erb'
 end
 
-template '/home/andrew/testTemplate.txt' do
-	source	'test.erb'
+template '/home/andrew/testtemplate.txt' do
+	source	'test.txt.erb'
 end
--- reset/recipes/directory.rb
+++ reset/recipes/directory.rb
@@ -1,109 +1,130 @@
-#Removes directories from Dawit's home Directory
+#
+# Cookbook:: reset
+# Recipe:: directory
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+# Robert
+directory '/home/robert/testDirectory' do
+  recursive true
+  action :delete
+end
+
+directory '/home/robert/Documents' do
+  recursive true
+  action :delete
+end
+
+directory '/home/robert/Desktop' do
+  recursive true
+  action :delete
+end
+
+directory '/home/robert/Applications' do
+  recursive true
+  action :delete
+end
+
+directory '/home/robert/Downloads' do
+  recursive true
+  action :delete
+end
+
+directory '/home/robert' do
+  recursive true
+  action :delete
+end
+
+# Deven
+directory '/home/deven/testDirectory' do
+  recursive true
+  action :delete
+end
+
+directory '/home/deven/Documents' do
+  recursive true
+  action :delete
+end
+
+directory '/home/deven/Desktop' do
+  recursive true
+  action :delete
+end
+
+directory '/home/deven/Applications' do
+  recursive true
+  action :delete
+end
+
+directory '/home/deven/Downloads' do
+  recursive true
+  action :delete
+end
+
+directory '/home/deven' do
+  recursive true
+  action :delete
+end
+
+# Andrew
+directory '/home/andrew/testDirectory' do
+  recursive true
+  action :delete
+end   
+
+directory '/home/andrew/Documents' do
+  recursive true
+  action :delete
+end
+
+directory '/home/andrew/Desktop' do
+  recursive true
+  action :delete
+end
+
+
+directory '/home/andrew/Applications' do
+  recursive true
+  action :delete
+end
+
+directory '/home/andrew/Downloads' do
+  recursive true
+  action :delete
+end
+
+directory '/home/andrew' do
+  recursive true
+  action :delete
+end
+
+# Dawit
 directory '/home/dawit/testDirectory' do
-    recursive true
-    action :remove
-  end
+  recursive true
+  action :delete
+end
 
 directory '/home/dawit/Documents' do
-    recursive true
-    action :remove
-  end
+  recursive true
+  action :delete
+end
 
 directory '/home/dawit/Desktop' do
-    recursive true
-    action :remove
-  end
+  recursive true
+  action :delete
+end
 
 directory '/home/dawit/Applications' do
-    recursive true
-    action :remove
-  end
+  recursive true
+  action :delete
+end
 
 directory '/home/dawit/Downloads' do
-    recursive true
-    action :remove
-  end
+  recursive true
+  action :delete
+end
 
-
- #Removes directories from Robert's home Directory
-directory '/home/robert/testDirectory' do
-    recursive true
-    action :remove
-  end
-directory '/home/robert/Documents' do
-    recursive true
-    action :remove
-   end
-directory '/home/robert/Desktop' do
-    recursive true
-    action :remove
-  end
-directory '/home/robert/Applications' do
-    recursive true
-    action :remove
-  end
-directory '/home/robert/Downloads' do
-    recursive true
-    action :remove
-  end
-
-
-  #Removes directories from Deven's home Directory
-directory '/home/deven/testDirectory' do
-    recursive true
-    action :remove
-  end
-
-  directory '/home/deven/Documents' do
-    recursive true
-    action :remove
-  end
-  
-  directory '/home/deven/Desktop' do
-    recursive true
-    action :remove
-  end
-  
-  directory '/home/deven/Applications' do
-    recursive true
-    action :remove
-  end
-  
-  directory '/home/deven/Downloads' do
-    recursive true
-    action :remove
-  end
-#Removes directories from Andrew's home Directory
-directory '/home/andrew/testDirectory' do
-    recursive true
-    action :remove
-  end
-
-  directory '/home/andrew/Documents' do
-    recursive true
-    action :remove
-  end
-  
-  
-  directory '/home/andrew/Desktop' do
-    recursive true
-    action :remove
-  end
-  
-  
-  directory '/home/andrew/Applications' do
-    recursive true
-    action :remove
-    end
-  
-  directory '/home/andrew/Downloads' do
-    recursive true
-    action :remove
-  end
-
-
-
-
-
-
-
+directory '/home/dawit' do
+  recursive true
+  action :delete
+end
