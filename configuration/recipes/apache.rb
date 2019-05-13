e0c98ba6bb7ace667d8f2e4276bc08c000849269
Robert <rquinones93@yahoo.com>
2019-05-13 13:43:36 -0700

    Changes for our presentation

--- configuration/recipes/apache.rb
+++ configuration/recipes/apache.rb
@@ -19,34 +19,42 @@ directory "/var/www/html/img" do
 end
 
 directory "/var/www/html/img/about" do
+  mode '0755' 
   action :create
 end
 
 directory "/var/www/html/img/team" do
+	mode '0755' 
   action :create
 end
 
 cookbook_file "var/www/html/img/header-bg.jpg" do
   source "img/header-bg.jpg"
+  mode '0755' 
+  action :create
 end
 
 cookbook_file "/var/www/html/img/about/1.jpg" do
   source "img/about/1.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/about/2.jpg" do
   source "img/about/2.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/about/3.jpg" do
   source "img/about/3.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/about/4.jpg" do
   source "img/about/4.jpg"
+  mode '0755' 
   action :create
 end
 
@@ -54,20 +62,24 @@ end
 # Adding Team member pictures
 cookbook_file "/var/www/html/img/team/0.jpg" do
   source "img/team/0.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/team/1.jpg" do
   source "img/team/1.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/team/2.jpg" do
   source "img/team/2.jpg"
+  mode '0755' 
   action :create
 end
 
 cookbook_file "/var/www/html/img/team/3.jpg" do
   source "img/team/3.jpg"
+  mode '0755' 
   action :create
 end
